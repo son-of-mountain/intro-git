27796674babae9951bcd934f9e520d16385b0106
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-17 20:24:12 +0100

    all checks

---
+++ module2/all_checks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+  
+import os
+import sys
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+
+main()
