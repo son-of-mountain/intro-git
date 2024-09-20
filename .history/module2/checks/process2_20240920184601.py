d8286134f6d137d8640daac43d47067558995034
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-20 18:59:28 +0100

    want to see if this commit will be erased from the log

--- .history/module2/checks/process2_20240920184601.py
+++ .history/module2/checks/process2_20240920184601.py
@@ -1,2 +1,1 @@
 # the following disclaimer in the documentation and/or other materials
-# provided with the distribution.
---
+++ .history/module2/checks/process2_20240920184631.py
@@ -0,0 +1,3 @@
+# the following disclaimer in the documentation and/or other materials
+
+# provided "as is" without warranty of any kind, either express or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose.
