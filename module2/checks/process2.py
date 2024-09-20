432ac183e90966826bc03668c2acb8f65405da8e
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-20 18:47:49 +0100

    Revert "added the second desc"
    This reverts commit 90a4d2e9a2527e127512d6d04a698155d36b0184.

--- module2/checks/process2.py
+++ module2/checks/process2.py
@@ -1,3 +1,1 @@
 # the following disclaimer in the documentation and/or other materials
-
-# provided "as is" without warranty of any kind, either express or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose.
