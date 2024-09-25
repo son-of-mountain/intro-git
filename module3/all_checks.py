5a11aac266d5ea3d7118d2a036307112cf2565dd
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-25 22:12:39 +0100

    added a simple network check

--- module3/all_checks.py
+++ module3/all_checks.py
@@ -2,6 +2,7 @@
   
 import os
 import sys
+import socket
 
 from check_free_space import check_reboot
 
@@ -17,10 +18,19 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_no_network():
+    """return True if it fails to resolve google's url , false otherwise."""
+    try:
+        socket.gethostbyname('google.com')
+        return False
+    except socket.gaierror:
+        return True
+
 def main():
     checks=[
         (check_reboot , "pending reboot"),
-        (check_reboot_full , "Root partition is full"),
+        (check_root_full , "Root partition is full"),
+        (check_no_network , "No internet connection"),
     ]
     allOk = True
     for check, name in checks:
